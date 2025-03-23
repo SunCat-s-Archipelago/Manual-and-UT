deb72a958441b482600ef141d5ac89ddbec095a8
SunCat <suncat.game@ya.ru>
2025-03-23 21:10:16 +0300

    fix loading without apmanual file

--- src/ManualClient.py
+++ src/ManualClient.py
@@ -143,8 +143,8 @@ class ManualContext(SuperContext):
     def suggested_game(self) -> str:
         if self.game:
             return self.game
-        from .Game import game_name  # This will at least give us the name of a manual they've installed
-        return Utils.persistent_load().get("client", {}).get("last_manual_game", game_name)
+        #from .Game import game_name  # This will at least give us the name of a manual they've installed
+        return Utils.persistent_load().get("client", {}).get("last_manual_game", "")
 
     def get_location_by_name(self, name) -> dict[str, Any]:
         location = self.location_table.get(name)
@@ -422,7 +422,11 @@ class ManualContext(SuperContext):
                 manuals = [w for w in AutoWorldRegister.world_types.keys() if "Manual_" in w]
                 manuals.sort()  # Sort by alphabetical order, not load order
                 self.manual_game_layout.add_widget(game_bar_label)
-                self.game_bar_text = Spinner(text=self.ctx.suggested_game, size_hint_y=None, height=dp(30),
+                if self.ctx.suggested_game != "":
+                    suggested_game = self.ctx.suggested_game
+                else:
+                    suggested_game = manuals[0]
+                self.game_bar_text = Spinner(text=suggested_game, size_hint_y=None, height=dp(30),
                                              sync_height=True,
                                              values=manuals, option_cls=GameSelectOption,
                                              dropdown_cls=GameSelectDropDown)
